997a7322bdf48bbd6a71e2b0371fe16e2fb3614c
labocho <labocho@penguinlab.jp>
2013-06-17 23:48:35 +0900

    アップロードを S3 に直接行うように

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,11 @@
+require "aws-sdk"
 class Item < ActiveRecord::Base
   RECENT = 20
 
   belongs_to :channel, touch: true
   scope :recent, -> { order('"published_at" DESC').limit(RECENT) }
 
-  attr_accessible :channel, :channel_id, :description, :published_at, :title, :content
+  attr_accessible :channel, :channel_id, :description, :published_at, :title, :content_filename
   mount_uploader :content, ItemUploader
 
   validates_presence_of :content
@@ -18,5 +19,28 @@ class Item < ActiveRecord::Base
 
   def to_s
     title
+  end
+
+  def content_filename=(v)
+    self[:content] = v
+  end
+
+  def as_json(options = {})
+    post = presigned_post
+    attributes.merge(
+      presigned_post: {
+        url: post.url.to_s,
+        fields: post.fields
+      }
+    )
+  end
+
+  def presigned_post
+    s3 = AWS::S3.new(
+      access_key_id: content.fog_credentials[:aws_access_key_id],
+      secret_access_key: content.fog_credentials[:aws_secret_access_key]
+    )
+    bucket = s3.buckets[content.fog_directory]
+    bucket.presigned_post(key: content.path)
   end
 end
