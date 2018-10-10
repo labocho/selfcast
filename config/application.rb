5fb88d45091b0b415be408a8ef491608614f18d0
labocho <labocho@penguinlab.jp>
2018-10-10 10:53:50 +0900

    update rails to 5.1

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Selfcast
       g.stylesheets false
       g.javascripts false
     end
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
